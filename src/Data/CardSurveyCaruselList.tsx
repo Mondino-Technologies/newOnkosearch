67cdc1b5ee5bdd4b35b40e4ffd0129d2227e83ac
ernest <ernest_everstov@mail.ru>
2022-02-25 15:47:49 +0900

    added fix register

--- src/Data/CardSurveyCaruselList.tsx
+++ src/Data/CardSurveyCaruselList.tsx
@@ -1,36 +1,36 @@
 import React from "react";
 
-import {FormattedMessage} from "react-intl";
+import { FormattedMessage } from "react-intl";
 
 const CardSurveyCaruselList = [
     {
         img: 'liver',
-        label: <FormattedMessage id={'screening_liver'}/>,
+        label: <FormattedMessage id={'screening_liver'} />,
         link: '/liver'
     },
     {
         img: 'gut',
-        label: <FormattedMessage id={'screening_colon'}/>,
+        label: <FormattedMessage id={'screening_colon'} />,
         link: '/gut'
     },
     {
         img: 'breath',
-        label: <FormattedMessage id={'screening_lungs'}/>,
+        label: <FormattedMessage id={'screening_lungs'} />,
         link: '/breath'
     },
     {
         img: 'breast',
-        label: <FormattedMessage id={'screening_mammary_gland'}/>,
+        label: <FormattedMessage id={'screening_mammary_gland'} />,
         link: '/breast'
     },
     {
         img: 'cervix',
-        label: <FormattedMessage id={'screening_cervix'}/>,
+        label: <FormattedMessage id={'screening_cervix'} />,
         link: '/cervix'
     },
     {
         img: 'prostata',
-        label: <FormattedMessage id={'screening_prostate'}/>,
+        label: <FormattedMessage id={'screening_prostate'} />,
         link: '/prostate'
     }
 ]
