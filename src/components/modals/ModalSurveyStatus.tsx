f24abc5421aa70a64e3d47414c83ba550b26bf1b
mrmainly <ernest_everstov@mail.ru>
2022-08-14 18:41:22 +0900

    added text to modal surveys

--- src/components/modals/ModalSurveyStatus.tsx
+++ src/components/modals/ModalSurveyStatus.tsx
@@ -1,36 +1,54 @@
-import React, { useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Button, Dialog, DialogContent, DialogActions, Typography } from '@mui/material'
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Typography,
+} from "@mui/material";
 
-import { StateContext, DispatchContext } from '../../store'
+import { StateContext, DispatchContext } from "../../store";
 
 const ModalSurveyStatus = () => {
-    const state = useContext(StateContext)
-    const dispatch = useContext(DispatchContext)
-    const navigate = useNavigate()
-    return (
-        <Dialog aria-labelledby="customized-dialog-title" open={state.modalSurveys.open}>
-            <div style={{ paddingTop: 5 }}>
-                <Typography variant="h5" gutterBottom style={{ paddingLeft: 15 }}>
-                    Ваш результат:
-                </Typography>
-                <DialogContent dividers>
-                    <Typography gutterBottom>
-                        {state.modalSurveys.description}
-                    </Typography>
-                </DialogContent>
-                <DialogActions>
-                    <Button variant="contained" color={'primary'} autoFocus onClick={() => {
-                        navigate('/surveys')
-                        dispatch({ type: 'modalSurvey', payload: { open: false, description: '' } })
-                    }}>
-                        Остальные анкеты
-                    </Button>
-                </DialogActions>
-            </div>
-        </Dialog>
-    );
-}
+  const state = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
+  const navigate = useNavigate();
+  return (
+    <Dialog
+      aria-labelledby="customized-dialog-title"
+      open={state.modalSurveys.open}
+    >
+      <div style={{ paddingTop: 5 }}>
+        <Typography variant="h5" gutterBottom style={{ paddingLeft: 15 }}>
+          Ваш результат:
+        </Typography>
+        <DialogContent dividers>
+          <Typography style={{ marginBottom: 15 }}>
+            Благодарим вас за успешное прошождение тестирование!
+          </Typography>
+          <Typography gutterBottom>{state.modalSurveys.description}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color={"primary"}
+            autoFocus
+            onClick={() => {
+              navigate("/surveys");
+              dispatch({
+                type: "modalSurvey",
+                payload: { open: false, description: "" },
+              });
+            }}
+          >
+            Остальные анкеты
+          </Button>
+        </DialogActions>
+      </div>
+    </Dialog>
+  );
+};
 
-export default ModalSurveyStatus
+export default ModalSurveyStatus;
